07f0adb72cd6e594c2b3c79e56d2dbe535a3b727
Jason Caldwell <jaswrks@o5p.me>
2024-02-01 00:14:17 -0500

    Project update. [p][robotic]

--- dev/.files/madrun/includes/node.cjs
+++ dev/.files/madrun/includes/node.cjs
@@ -6,22 +6,20 @@
  * @note Instead of editing here, please review <https://github.com/clevercanyon/skeleton>.
  */
 
-const originalEmitter = process.emit; // Original emitter.
-
 /**
  * Filters Node warnings.
  */
-process.emit = (event, error) => {
-    if ('warning' === event && error instanceof Error && 'ExperimentalWarning' === error.name && error.message) {
-        if (error.message.includes('Web Crypto API algorithm is an experimental feature')) {
+process.on('warning', (warning) => {
+    if ('ExperimentalWarning' === warning.name && warning.message) {
+        if (warning.message.includes('Web Crypto API algorithm is an experimental feature')) {
             return false; // Web Crypto is a thing. Ok to suppress.
         }
         if (
-            error.message.includes('Import assertions are not a stable feature') || //
-            error.message.includes('Importing JSON modules is an experimental feature')
+            warning.message.includes('Import assertions are not a stable feature') || //
+            warning.message.includes('Importing JSON modules is an experimental feature')
         ) {
             return false; // JSON imports are a thing. Ok to suppress.
         }
     }
-    return originalEmitter.apply(process, [event, error]);
-};
+    console.warn(warning);
+});
