039cd0bdc2f88c3a8893a22f8ad44b6b7779ec28
Jason Caldwell <jaswrks@o5p.me>
2024-02-25 01:05:57 -0500

    Project update. [p][robotic]

--- dev-types.d.ts
+++ dev-types.d.ts
@@ -9,31 +9,62 @@
  */
 
 /**
- * Declares Vite global app constants.
+ * Declares global scope.
  */
-declare const $$__APP_PKG_NAME__$$: string;
-declare const $$__APP_PKG_VERSION__$$: string;
+declare namespace globalThis {
+    /**
+     * Imports utility types.
+     */
+    import { $type } from '@clevercanyon/utilities';
 
-declare const $$__APP_BUILD_TIME_YMD__$$: string;
-declare const $$__APP_BUILD_TIME_SQL__$$: string;
-declare const $$__APP_BUILD_TIME_ISO__$$: string;
-declare const $$__APP_BUILD_TIME_STAMP__$$: string;
+    /**
+     * Declares Vite global app constants.
+     */
+    const $$__APP_PKG_NAME__$$: string;
+    const $$__APP_PKG_VERSION__$$: string;
 
-declare const $$__APP_BASE_URL__$$: string;
-declare const $$__APP_BASE_URL_RESOLVED_NTS__$$: string;
+    const $$__APP_BUILD_TIME_YMD__$$: string;
+    const $$__APP_BUILD_TIME_SQL__$$: string;
+    const $$__APP_BUILD_TIME_ISO__$$: string;
+    const $$__APP_BUILD_TIME_STAMP__$$: string;
+
+    const $$__APP_BASE_URL__$$: string;
+    const $$__APP_BASE_URL_RESOLVED_NTS__$$: string;
+
+    /**
+     * Declares prefresh API in Vite plugin.
+     */
+    const __PREFRESH__: object;
+
+    /**
+     * Declares PWA install event, which we implement for SPAs.
+     */
+    let pwaInstallEvent: Event & { prompt: () => void };
+
+    /**
+     * Defines missing `entries()` on FormData.
+     */
+    interface FormData {
+        entries(): IterableIterator<[key: string, value: string | Blob]>;
+    }
+
+    /**
+     * Defines missing `entries()` on Headers.
+     */
+    interface Headers {
+        entries(): IterableIterator<[key: string, value: string]>;
+    }
+
+    /**
+     * Defines a typed cause on native error interface.
+     */
+    interface Error {
+        cause?: $type.ErrorCause;
+    }
+}
 
 /**
- * Declares prefresh API in Vite plugin.
- */
-declare var __PREFRESH__: object;
-
-/**
- * Declares PWA install event, which we implement for SPAs.
- */
-declare var pwaInstallEvent: Event & { prompt: () => void };
-
-/**
- * Declares virtual brand config file.
+ * Declares virtual brand config module.
  */
 declare module 'virtual:brand/config' {
     import { $type } from '@clevercanyon/utilities';
@@ -46,20 +77,6 @@ declare module 'virtual:brand/config' {
 declare module 'cloudflare:sockets' {
     import { $type } from '@clevercanyon/utilities';
     export function connect(address: string | $type.cfw.SocketAddress, options?: $type.cfw.SocketOptions): $type.cfw.Socket;
-}
-
-/**
- * Defines missing `entries()` on FormData.
- */
-interface FormData {
-    entries(): IterableIterator<[key: string, value: string | Blob]>;
-}
-
-/**
- * Defines missing `entries()` on Headers.
- */
-interface Headers {
-    entries(): IterableIterator<[key: string, value: string]>;
 }
 
 /*
